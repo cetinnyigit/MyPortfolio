f1b77dff57bf801a5154c29a85dec89e14e306a6
Yiğit Çetin <yigitcetin04@hotmail.com>
2025-04-11 05:54:25 +0300

    Admin Paneli Tamamlandı

---
+++ MyPortfolio/Controllers/ExperienceController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult ExperienceList()
+        {
+
+            var values = context.Experiences.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateExperience()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        public IActionResult DeleteExperience(int id)
+        {
+            var experience = context.Experiences.Find(id);
+            context.Experiences.Remove(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var experience = context.Experiences.Find(id);
+            return View(experience);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience experience)
+        {
+            context.Experiences.Update(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        
+    }
+}
